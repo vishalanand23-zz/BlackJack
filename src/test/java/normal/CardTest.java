b81ac445dc6052b554f0945675c9ee46b7f8ab66
Vishal Anand <vishalanand23@gmail.com>
2012-06-28 15:18:35 +0530

    tests written for Card class

--- src/test/java/normal/CardTest.java
+++ src/test/java/normal/CardTest.java
@@ -1,4 +1,22 @@
 package normal;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 public class CardTest {
+
+    @Test
+    public void cardValueLessThanTen() {
+        Card card = new Card(2, 2);
+        int num = card.value();
+        assertEquals(2, num);
+    }
+
+    @Test
+    public void cardValueMoreThanTen() {
+        Card card = new Card(12, 2);
+        int num = card.value();
+        assertEquals(10, num);
+    }
 }
