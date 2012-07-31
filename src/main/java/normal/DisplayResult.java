f70998b4e32f99a04322f888c8e521c93460f29b
Vishal Anand <vishalanand23@gmail.com>
2012-07-31 14:11:08 +0530

    DisplyResultTest added

--- src/main/java/normal/DisplayResult.java
+++ src/main/java/normal/DisplayResult.java
@@ -1,17 +1,18 @@
 package normal;
 
 public class DisplayResult {
+
     public Result result(Player better, Player dealer) {
         int playerSum = better.value();
         int dealerSum = dealer.value();
         if (playerSum == -1) return Result.DEALER_WIN;
-        if (dealerSum == -1) return Result.PLAYER_WIN;
-        if (playerSum > dealerSum) return Result.PLAYER_WIN;
+        if (dealerSum == -1) return Result.GAMBLER_WIN;
+        if (playerSum > dealerSum) return Result.GAMBLER_WIN;
         if (dealerSum > playerSum) return Result.DEALER_WIN;
         else return Result.PUSH;
     }
 
     public enum Result {
-        PLAYER_WIN, DEALER_WIN, PUSH
+        GAMBLER_WIN, DEALER_WIN, PUSH
     }
 }
---
+++ src/test/java/normal/DisplayResultTest.java
@@ -0,0 +1,77 @@
+package normal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DisplayResultTest {
+
+    private Player gambler;
+    private Player dealer;
+    private PlayerTest.DealerStub cardsDealer;
+    private DisplayResult displayResult = new DisplayResult();
+
+
+    @Before
+    public void setUp() {
+        cardsDealer = new PlayerTest.DealerStub();
+        gambler = new Player(cardsDealer);
+        dealer = new Player(cardsDealer);
+    }
+
+    @Test
+    public void gamblerShouldWin() {
+        dealCardsToPlayer(gambler, cardsDealer, 9, 2);
+        dealCardsToPlayer(gambler, cardsDealer, 8, 1);
+        dealCardsToPlayer(gambler, cardsDealer, 3, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 1, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 8, 3);
+        assertEquals(DisplayResult.Result.GAMBLER_WIN, displayResult.result(gambler, dealer));
+    }
+
+    @Test
+    public void dealerShouldWin() {
+        dealCardsToPlayer(gambler, cardsDealer, 9, 2);
+        dealCardsToPlayer(gambler, cardsDealer, 8, 1);
+        dealCardsToPlayer(dealer, cardsDealer, 1, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 8, 3);
+        assertEquals(DisplayResult.Result.DEALER_WIN, displayResult.result(gambler, dealer));
+    }
+
+    @Test
+    public void dealerBustGamblerShouldWin() {
+        dealCardsToPlayer(gambler, cardsDealer, 9, 2);
+        dealCardsToPlayer(dealer, cardsDealer, 12, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 11, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 8, 3);
+        assertEquals(DisplayResult.Result.GAMBLER_WIN, displayResult.result(gambler, dealer));
+    }
+
+    @Test
+    public void gamblerBustDealerShouldWin() {
+        dealCardsToPlayer(gambler, cardsDealer, 9, 2);
+        dealCardsToPlayer(gambler, cardsDealer, 8, 1);
+        dealCardsToPlayer(gambler, cardsDealer, 5, 1);
+        dealCardsToPlayer(dealer, cardsDealer, 12, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 11, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 8, 3);
+        assertEquals(DisplayResult.Result.DEALER_WIN, displayResult.result(gambler, dealer));
+    }
+
+    @Test
+    public void push() {
+        dealCardsToPlayer(gambler, cardsDealer, 9, 2);
+        dealCardsToPlayer(gambler, cardsDealer, 8, 1);
+        dealCardsToPlayer(dealer, cardsDealer, 12, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 4, 3);
+        dealCardsToPlayer(dealer, cardsDealer, 3, 3);
+        assertEquals(DisplayResult.Result.PUSH, displayResult.result(gambler, dealer));
+    }
+
+    private void dealCardsToPlayer(Player p, PlayerTest.DealerStub dealer, int number, int suit) {
+        dealer.setNumber(number);
+        dealer.setSuit(suit);
+        p.deal();
+    }
+}
