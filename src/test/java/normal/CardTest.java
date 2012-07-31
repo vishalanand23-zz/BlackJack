aba0e6476cedbb39e41853fe907dd616fde5f1f9
Vishal Anand <vishalanand23@gmail.com>
2012-07-31 12:59:11 +0530

    test for constructor added in CardTest

--- src/test/java/normal/CardTest.java
+++ src/test/java/normal/CardTest.java
@@ -3,8 +3,37 @@ package normal;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
 
 public class CardTest {
+
+    @Test
+    public void throwErrorIfCardNumberGreaterThan13OrLessThan0() {
+        try {
+            new Card(0, 3);
+            fail("An Exception should have been thrown for number > 13");
+        } catch (Card.CardGenerationError error) {
+        }
+        try {
+            new Card(14, 1);
+            fail("An Exception should have been thrown for number < 1");
+        } catch (Card.CardGenerationError error) {
+        }
+    }
+
+    @Test
+    public void throwErrorIfCardSuitGreaterThan4OrLessThan0() {
+        try {
+            new Card(3, 5);
+            fail("An Exception should have been thrown for suit > 4");
+        } catch (Card.CardGenerationError error) {
+        }
+        try {
+            new Card(6, 0);
+            fail("An Exception should have been thrown for suit < 1");
+        } catch (Card.CardGenerationError error) {
+        }
+    }
 
     @Test
     public void cardValueLessThanTen() {
