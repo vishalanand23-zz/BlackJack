e82b536ac37b409d6fcd01106289cf4481760a7f
Vishal Anand <vishalanand23@gmail.com>
2012-08-02 01:43:30 +0530

    added insertion to database when result is published

--- src/main/java/normal/DisplayResult.java
+++ src/main/java/normal/DisplayResult.java
@@ -1,17 +1,52 @@
 package normal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DisplayResult {
+
+    private Dao dao = new Dao();
+
     public Result result(Player better, Player dealer) {
         int playerSum = better.value();
         int dealerSum = dealer.value();
-        if (playerSum == -1) return Result.DEALER_WIN;
-        if (dealerSum == -1) return Result.PLAYER_WIN;
-        if (playerSum > dealerSum) return Result.PLAYER_WIN;
-        if (dealerSum > playerSum) return Result.DEALER_WIN;
-        else return Result.PUSH;
+        Result gameResult;
+        if (playerSum == -1) gameResult = Result.DEALER_WIN;
+        else if (dealerSum == -1) gameResult = Result.PLAYER_WIN;
+        else if (playerSum > dealerSum) gameResult = Result.PLAYER_WIN;
+        else if (dealerSum > playerSum) gameResult = Result.DEALER_WIN;
+        else gameResult = Result.PUSH;
+        dao.insert(gameResult);
+        return gameResult;
     }
 
     public enum Result {
         PLAYER_WIN, DEALER_WIN, PUSH
+    }
+
+    public static class Dao {
+        private Connection con;
+        private PreparedStatement st;
+
+        public Dao() {
+            try {
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/blackjack", "root", "");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void insert(Result gameResult) {
+            try {
+                st = con.prepareStatement("insert into result_summary(result) values(?)");
+                st.setString(1, gameResult.toString());
+                st.executeUpdate();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
